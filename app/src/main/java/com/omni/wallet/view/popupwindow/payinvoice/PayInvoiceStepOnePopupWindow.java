c08543ca67f77537dec82f6f699a93d9b315a7c4
Tongchanghui <410525366@qq.com>
2022-10-20 16:56:00 +0800

    Merge pay invoice mould all step to invoice step one 将pay invoice模块中的所有步骤到step one

--- app/src/main/java/com/omni/wallet/view/popupwindow/payinvoice/PayInvoiceStepOnePopupWindow.java
+++ app/src/main/java/com/omni/wallet/view/popupwindow/payinvoice/PayInvoiceStepOnePopupWindow.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 import com.omni.wallet.R;
 import com.omni.wallet.baselibrary.view.BasePopWindow;
@@ -18,6 +19,7 @@ public class PayInvoiceStepOnePopupWindow {
     private Context mContext;
     private BasePopWindow mBasePopWindow;
     PayInvoiceStepTwoPopupWindow mPayInvoiceStepTwoPopupWindow;
+    RelativeLayout shareLayout;
 
     public PayInvoiceStepOnePopupWindow(Context context) {
         this.mContext = context;
@@ -27,7 +29,7 @@ public class PayInvoiceStepOnePopupWindow {
     public void show(final View view) {
         if (mBasePopWindow == null) {
             mBasePopWindow = new BasePopWindow(mContext);
-            View rootView = mBasePopWindow.setContentView(R.layout.layout_popupwindow_pay_invoice_stepone);
+            final View rootView = mBasePopWindow.setContentView(R.layout.layout_popupwindow_pay_invoice_stepone);
             mBasePopWindow.setWidth(WindowManager.LayoutParams.MATCH_PARENT);
             mBasePopWindow.setHeight(WindowManager.LayoutParams.MATCH_PARENT);
 //            mBasePopWindow.setBackgroundDrawable(new ColorDrawable(0xD1123A50));
@@ -40,16 +42,84 @@ public class PayInvoiceStepOnePopupWindow {
                 }
             });
             // 点击next
-            rootView.findViewById(R.id.layout_next).setOnClickListener(new View.OnClickListener() {
+            rootView.findViewById(R.id.layout_next_to_two).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    rootView.findViewById(R.id.lv_pay_invoice_step_two).setVisibility(View.VISIBLE);
+                    rootView.findViewById(R.id.lv_pay_invoice_step_one).setVisibility(View.GONE);
+                }
+            });
+            // 点击back
+            rootView.findViewById(R.id.layout_back_to_one).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    rootView.findViewById(R.id.lv_pay_invoice_step_two).setVisibility(View.GONE);
+                    rootView.findViewById(R.id.lv_pay_invoice_step_one).setVisibility(View.VISIBLE);
+                }
+            });
+            // 点击pay
+            rootView.findViewById(R.id.layout_pay).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    rootView.findViewById(R.id.lv_pay_invoice_step_two).setVisibility(View.GONE);
+//                    rootView.findViewById(R.id.lv_pay_invoice_step_three).setVisibility(View.VISIBLE);
+                    rootView.findViewById(R.id.lv_pay_invoice_step_failed).setVisibility(View.VISIBLE);
+                    rootView.findViewById(R.id.layout_cancel).setVisibility(View.GONE);
+                    rootView.findViewById(R.id.layout_close).setVisibility(View.VISIBLE);
+
+                }
+            });
+            shareLayout = rootView.findViewById(R.id.layout_share);
+            rootView.findViewById(R.id.layout_parent).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    shareLayout.setVisibility(View.GONE);
+                }
+            });
+            // 点击back
+            rootView.findViewById(R.id.layout_back_to_two).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    rootView.findViewById(R.id.lv_pay_invoice_step_two).setVisibility(View.VISIBLE);
+                    rootView.findViewById(R.id.lv_pay_invoice_step_failed).setVisibility(View.GONE);
+                    rootView.findViewById(R.id.layout_cancel).setVisibility(View.VISIBLE);
+                    rootView.findViewById(R.id.layout_close).setVisibility(View.GONE);
+                }
+            });
+            // 点击share to
+            rootView.findViewById(R.id.layout_share_to).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    System.out.println(shareLayout.toString());
+                    shareLayout.setVisibility(View.VISIBLE);
+                }
+            });
+            // 点击facebook
+            rootView.findViewById(R.id.iv_facebook_share).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mBasePopWindow.dismiss();
-                    mPayInvoiceStepTwoPopupWindow = new PayInvoiceStepTwoPopupWindow(mContext);
-                    mPayInvoiceStepTwoPopupWindow.show(view);
+                    shareLayout.setVisibility(View.GONE);
+                }
+            });
+            // 点击twitter
+            rootView.findViewById(R.id.iv_twitter_share).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mBasePopWindow.dismiss();
+                    shareLayout.setVisibility(View.GONE);
                 }
             });
             // 点击底部cancel
             rootView.findViewById(R.id.layout_cancel).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mBasePopWindow.dismiss();
+                }
+            });
+
+            // 点击底部close
+            rootView.findViewById(R.id.layout_close).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mBasePopWindow.dismiss();
