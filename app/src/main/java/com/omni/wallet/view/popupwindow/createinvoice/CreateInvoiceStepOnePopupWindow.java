8d83fe9adf47488cb817c75f8164ba95a7f7e40f
Tongchanghui <410525366@qq.com>
2022-10-21 12:38:45 +0800

    合并create invoice 步骤到 step one Merge create invoice all page to step one

--- app/src/main/java/com/omni/wallet/view/popupwindow/createinvoice/CreateInvoiceStepOnePopupWindow.java
+++ app/src/main/java/com/omni/wallet/view/popupwindow/createinvoice/CreateInvoiceStepOnePopupWindow.java
@@ -5,6 +5,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 
 import com.omni.wallet.R;
 import com.omni.wallet.baselibrary.view.BasePopWindow;
@@ -20,8 +21,7 @@ public class CreateInvoiceStepOnePopupWindow {
 
     private Context mContext;
     private BasePopWindow mBasePopWindow;
-    CreateInvoiceFailedPopupWindow mCreateInvoiceFailedPopupWindow;
-    CreateInvoiceSuccessPopupWindow mCreateInvoiceSuccessPopupWindow;
+    RelativeLayout shareLayout;
 
     public CreateInvoiceStepOnePopupWindow(Context context) {
         this.mContext = context;
@@ -30,35 +30,135 @@ public class CreateInvoiceStepOnePopupWindow {
     public void show(final View view) {
         if (mBasePopWindow == null) {
             mBasePopWindow = new BasePopWindow(mContext);
-            View rootView = mBasePopWindow.setContentView(R.layout.layout_popupwindow_create_invoice_stepone);
+            final View rootView = mBasePopWindow.setContentView(R.layout.layout_popupwindow_create_invoice_stepone);
             mBasePopWindow.setWidth(WindowManager.LayoutParams.MATCH_PARENT);
             mBasePopWindow.setHeight(WindowManager.LayoutParams.MATCH_PARENT);
 //            mBasePopWindow.setBackgroundDrawable(new ColorDrawable(0xD1123A50));
             mBasePopWindow.setAnimationStyle(R.style.popup_anim_style);
-            // 设置进度条
+            /**
+             * @描述： 设置进度条
+             * @desc: set progress bar
+             */
             ProgressBar mProgressBar = rootView.findViewById(R.id.progressbar);
             float barValue = (float) ((double) 100 / (double) 600);
             mProgressBar.setProgress((int) (barValue * 100f));
-            // 点击back
-            rootView.findViewById(R.id.layout_back).setOnClickListener(new View.OnClickListener() {
+            /**
+             * @描述： 点击Back
+             * @desc: click back button
+             */
+            rootView.findViewById(R.id.layout_back_to_none).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mBasePopWindow.dismiss();
                 }
             });
-            // 点击next
+            /**
+             * @描述： 点击Next
+             * @desc: click next button
+             */
             rootView.findViewById(R.id.layout_next).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    mBasePopWindow.dismiss();
-//                    mCreateInvoiceFailedPopupWindow = new CreateInvoiceFailedPopupWindow(mContext);
-//                    mCreateInvoiceFailedPopupWindow.show(view);
-                    mCreateInvoiceSuccessPopupWindow = new CreateInvoiceSuccessPopupWindow(mContext);
-                    mCreateInvoiceSuccessPopupWindow.show(view);
+                    rootView.findViewById(R.id.lv_create_invoice_step_one).setVisibility(View.GONE);
+//                    rootView.findViewById(R.id.lv_create_invoice_failed).setVisibility(View.VISIBLE);
+                    rootView.findViewById(R.id.lv_create_invoice_success).setVisibility(View.VISIBLE);
+                    rootView.findViewById(R.id.layout_cancel).setVisibility(View.GONE);
+                    rootView.findViewById(R.id.layout_close).setVisibility(View.VISIBLE);
                 }
             });
-            // 点击底部cancel
+            /**
+             * @描述： 点击Back
+             * @desc: click back button
+             */
+            rootView.findViewById(R.id.layout_back_to_one).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    rootView.findViewById(R.id.lv_create_invoice_step_one).setVisibility(View.VISIBLE);
+                    rootView.findViewById(R.id.lv_create_invoice_failed).setVisibility(View.GONE);
+                    rootView.findViewById(R.id.layout_cancel).setVisibility(View.VISIBLE);
+                    rootView.findViewById(R.id.layout_close).setVisibility(View.GONE);
+                }
+            });
+            /**
+             * @描述： 点击失败页share to
+             * @desc: click share to button in failed page
+             */
+            rootView.findViewById(R.id.layout_share_to).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+            /**
+             * @描述： 点击Back
+             * @desc: click back button
+             */
+            rootView.findViewById(R.id.layout_back).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    rootView.findViewById(R.id.lv_create_invoice_step_one).setVisibility(View.VISIBLE);
+                    rootView.findViewById(R.id.lv_create_invoice_success).setVisibility(View.GONE);
+                    rootView.findViewById(R.id.layout_cancel).setVisibility(View.VISIBLE);
+                    rootView.findViewById(R.id.layout_close).setVisibility(View.GONE);
+                }
+            });
+
+            /**
+             * for success page
+             * 成功页面
+             */
+            shareLayout = rootView.findViewById(R.id.layout_share_success);
+            rootView.findViewById(R.id.layout_parent).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    shareLayout.setVisibility(View.GONE);
+                }
+            });
+            /**
+             * @描述： 点击成功页 share to
+             * @desc: click share to button in success page
+             */
+            rootView.findViewById(R.id.layout_share_to_success).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    shareLayout.setVisibility(View.VISIBLE);
+                }
+            });
+            /**
+             * @描述： 点击成功页 facebook
+             * @desc: click facebook button in success page
+             */
+            rootView.findViewById(R.id.iv_facebook_share).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    shareLayout.setVisibility(View.GONE);
+                }
+            });
+            /**
+             * @描述： 点击成功页 twitter
+             * @desc: click twitter button in success page
+             */
+            rootView.findViewById(R.id.iv_twitter_share).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    shareLayout.setVisibility(View.GONE);
+                }
+            });
+            /**
+             * @描述： 点击cancel
+             * @desc: click cancel button
+             */
             rootView.findViewById(R.id.layout_cancel).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mBasePopWindow.dismiss();
+                }
+            });
+            /**
+             * @描述： 点击close
+             * @desc: click close button
+             */
+            rootView.findViewById(R.id.layout_close).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     mBasePopWindow.dismiss();
