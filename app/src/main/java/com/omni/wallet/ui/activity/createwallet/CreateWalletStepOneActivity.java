ba8c44cad184c2e82f5be3a92e5c2591e0c575f0
Tongchanghui <410525366@qq.com>
2022-11-03 15:53:03 +0800

    Change lnd start mode to create wallet start at gen seed Change init view mode for async in create step one
    变更lnd的开始方式为从获取seeds开始
    变更创建钱包第一步页面中渲染页面为异步

--- app/src/main/java/com/omni/wallet/ui/activity/createwallet/CreateWalletStepOneActivity.java
+++ app/src/main/java/com/omni/wallet/ui/activity/createwallet/CreateWalletStepOneActivity.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.omni.wallet.R;
 import com.omni.wallet.base.AppBaseActivity;
 import com.omni.wallet.utils.CopyUtil;
@@ -15,13 +17,24 @@ import com.omni.wallet.utils.NumberFormatter;
 import com.omni.wallet.view.dialog.LoadingDialog;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.OnClick;
+import lndmobile.Callback;
+import lndmobile.Lndmobile;
+import lnrpc.LightningOuterClass;
+import lnrpc.Walletunlocker;
 
 public class CreateWalletStepOneActivity extends AppBaseActivity {
-    String[] seedArray = {"about", "gun", "blind", "method", "addict", "scrub", "red", "risbon", "such", "kitchen", "prevent", "gap", "super", "risk", "survey", "cable", "image", "weather", "prize", "item", "between", "moral", "worth", "must"};
+    List <String> seedArray = new ArrayList();
     String seedsString = "";
     Context ctx = CreateWalletStepOneActivity.this;
     LoadingDialog mLoadingDialog;
+
+    @BindView(R.id.seed_content)
+    LinearLayout lvSeedContent;
 
     @Override
     protected Drawable getWindowBackground() {
@@ -37,19 +50,21 @@ public class CreateWalletStepOneActivity extends AppBaseActivity {
 
     @Override
     protected void initView() {
-
+        mLoadingDialog = new LoadingDialog(mContext);
+        
     }
 
     @Override
     protected void initData() {
         createSeeds();
-        initTvForSeeds();
     }
     
     private void initTvForSeeds(){
-        for (int idx = 0;idx<seedArray.length;idx++){
-            seedsString = seedsString + seedArray[idx]+ " ";
+        System.out.println("--------------------------------Start init tv for seeds--------------------------------");
+        for (int idx = 0;idx<seedArray.size();idx++){
+            seedsString = seedsString + seedArray.get(idx)+ " ";
         }
+        System.out.println("--------------------------------"+seedsString+"--------------------------------");
         /**
          * 使用SharedPreferences 对象，在生成seeds时候将seeds备份到本地文件
          * Use SharedPreferences Class to back up seeds to local file,when create seeds.
@@ -59,8 +74,10 @@ public class CreateWalletStepOneActivity extends AppBaseActivity {
         editor.putString("seeds",seedsString);
         editor.commit();
 
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.seed_content);
+        
+        
         for (int rowNum = 1; rowNum <= 8; rowNum++) {
+            System.out.println("--------------------------------row number:"+rowNum+"--------------------------------");
             RelativeLayout rowContent = new RelativeLayout(this);
             RelativeLayout.LayoutParams rowContentLayoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
             rowContent.setLayoutParams(rowContentLayoutParams);
@@ -73,8 +90,9 @@ public class CreateWalletStepOneActivity extends AppBaseActivity {
             rowInnerContent.setLayoutParams(rowInnerContentParams);
 
             for (int itemNum = 1; itemNum <= 3; itemNum++) {
+                
                 int noNum = (rowNum - 1) * 3 + itemNum;
-
+                System.out.println("--------------------------------item number:"+noNum+"--------------------------------");
                 LinearLayout itemInnerContent = new LinearLayout(this);
                 LinearLayout.LayoutParams itemInnerContentParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f);
                 itemInnerContent.setOrientation(LinearLayout.HORIZONTAL);
@@ -91,7 +109,7 @@ public class CreateWalletStepOneActivity extends AppBaseActivity {
 
                 TextView itemSeeWidget = new TextView(this);
                 RelativeLayout.LayoutParams itemSeedWidgetParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
-                itemSeeWidget.setText(seedArray[noNum - 1]);
+                itemSeeWidget.setText(seedArray.get(noNum-1));
                 itemSeeWidget.setTextColor(getResources().getColor(R.color.color_black));
                 itemSeeWidget.setTextSize(20.0f);
                 itemSeeWidget.setLayoutParams(itemSeedWidgetParams);
@@ -99,14 +117,47 @@ public class CreateWalletStepOneActivity extends AppBaseActivity {
                 itemInnerContent.addView(itemNoWidget);
                 itemInnerContent.addView(itemSeeWidget);
                 rowInnerContent.addView(itemInnerContent);
+                System.out.println("--------------------------------item number:"+noNum+"over--------------------------------");
             }
-
+            
             rowContent.addView(rowInnerContent);
-            linearLayout.addView(rowContent);
+            System.out.println("--------------------------------row number:"+rowNum+"rowInner add over--------------------------------");
+            System.out.println("--------------------------------row number:"+rowNum+lvSeedContent.toString()+"rowInner add start--------------------------------");
+            lvSeedContent.addView(rowContent);
+            System.out.println("--------------------------------row number:"+rowNum+lvSeedContent.toString()+"rowInner add over--------------------------------");
+            System.out.println("--------------------------------row number:"+rowNum+"over--------------------------------");
         }
+        System.out.println("--------------------------------End init tv for seeds--------------------------------");
     }
 
     private void createSeeds() {
+        Log.e("create error:","123456789");
+        mLoadingDialog.show();
+        Walletunlocker.GenSeedRequest genSeedRequest = Walletunlocker.GenSeedRequest.newBuilder().build();
+        Lndmobile.genSeed(genSeedRequest.toByteArray(), new Callback() {
+            @Override
+            public void onError(Exception e) {
+                Log.e("create error:",e.toString());
+                mLoadingDialog.dismiss();
+            }
+
+            @Override
+            public void onResponse(byte[] bytes) {
+                try{
+                    Walletunlocker.GenSeedResponse genSeedResponse = Walletunlocker.GenSeedResponse.parseFrom(bytes);
+                    List genSeedResponseList =  genSeedResponse.getCipherSeedMnemonicList();
+                    for (int i = 0; i<genSeedResponseList.size();i++){
+                        seedArray.add(genSeedResponseList.get(i).toString());
+                        Log.e("seed item",genSeedResponseList.get(i).toString());
+                    }
+                    initTvForSeeds();
+                    mLoadingDialog.dismiss();
+                }catch (InvalidProtocolBufferException e){
+                    e.printStackTrace();
+                    mLoadingDialog.dismiss();
+                }
+            }
+        });
     }
 
 
