20c2542cacf212e9c49072918bd0a5617b3822bc
progressorTCH <410525366@qq.com>
2023-03-17 12:22:26 +0800

    change code description

--- app/src/main/java/com/omni/wallet/utils/KeyboardScrollView.java
+++ app/src/main/java/com/omni/wallet/utils/KeyboardScrollView.java
@@ -9,22 +9,22 @@ public class KeyboardScrollView {
     public static void controlKeyboardLayout(final View root, final View scrollToView) {
         root.getViewTreeObserver().addOnGlobalLayoutListener(() -> {
             Rect rect = new Rect();
-            //获取root在窗体的可视区域
+            //Get the visible area of ​​root in the form
             root.getWindowVisibleDisplayFrame(rect);
             //获取root在窗体的不可视区域高度(被其他View遮挡的区域高度)
             int rootInvisibleHeight = root.getRootView().getHeight() - rect.bottom;
             if (Math.abs(rootInvisibleHeight - recordVisibleRec) > 200) {
-                //若不可视区域高度大于200，则键盘显示
+                //If the height of the invisible area is greater than 200, the keyboard will be displayed
                 if (rootInvisibleHeight > 200) {
                     int[] location = new int[2];
-                    //获取scrollToView在窗体的坐标
+                    //Get the coordinates of scrollToView in the form
                     scrollToView.getLocationInWindow(location);
-                    //计算root滚动高度，使scrollToView在可见区域
+                    //Calculate the root scroll height so that scrollToView is in the visible area
                     int srollHeight = (location[1] + scrollToView.getHeight()) - rect.bottom;
                     srollHeight = srollHeight < 0 ? 0 : srollHeight;
                     root.scrollTo(0, srollHeight);
                 } else {
-                    //键盘隐藏
+                    //hide keyboard
                     root.scrollTo(0, 0);
                 }
             }
