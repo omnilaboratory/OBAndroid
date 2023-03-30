230b31cfc18c8e1be741b94bbe24f63e82bba191
healergyl <175641710@qq.com>
2023-03-30 19:44:51 +0800

    Optimize some functions.

--- app/src/main/java/com/omni/wallet/utils/CheckRules.java
+++ app/src/main/java/com/omni/wallet/utils/CheckRules.java
@@ -6,7 +6,7 @@ public class CheckRules {
 
     public static Boolean checkSeedString(String seedsString){
         boolean flag = false;
-        String reg = "[a-zA-z\\s]*";
+        String reg = "[a-zA-Z\\s]*";
         flag = Pattern.matches(reg,seedsString);
         return flag;
     }
