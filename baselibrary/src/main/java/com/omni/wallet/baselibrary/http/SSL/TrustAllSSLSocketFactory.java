f29f2300f98fa01153173e8e207396d81d4b136c
healergyl <175641710@qq.com>
2023-04-03 14:05:59 +0800

    Restore some functions.

--- baselibrary/src/main/java/com/omni/wallet/baselibrary/http/SSL/TrustAllSSLSocketFactory.java
+++ baselibrary/src/main/java/com/omni/wallet/baselibrary/http/SSL/TrustAllSSLSocketFactory.java
@@ -1,13 +1,10 @@
 package com.omni.wallet.baselibrary.http.SSL;
 
 import java.security.SecureRandom;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 
 
 public class TrustAllSSLSocketFactory {
@@ -20,23 +17,7 @@ public class TrustAllSSLSocketFactory {
         SSLSocketFactory mSSLSocketFactory = null;
         try {
             SSLContext mSSLContext = SSLContext.getInstance("TLS");
-            TrustManager[] trustManagers = {new X509TrustManager() {
-                @Override
-                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-
-                }
-
-                @Override
-                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-
-                }
-
-                @Override
-                public X509Certificate[] getAcceptedIssuers() {
-                    return new X509Certificate[0];
-                }
-            }};
-            mSSLContext.init(null, trustManagers, new SecureRandom());
+            mSSLContext.init(null, new TrustManager[]{new TrustAllManager()}, new SecureRandom());
             mSSLSocketFactory = mSSLContext.getSocketFactory();
         } catch (Exception e) {
             e.printStackTrace();
