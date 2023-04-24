dc5d0ae0e369d6d37c604f54683f55a87330e742
progressorTCH <410525366@qq.com>
2023-04-24 22:18:41 +0800

    Add remote node database 添加远程节点数据库 Add remote node database read method 添加远程节点数据库读取方法 Added a remote node database clearing method 新增远程节点数据库清空方法 Add remote node database storage method 添加远程节点数据库存储方式 Increase the remote node dynamic request method 增加远程节点动态请求方法 Add the logic to update the database after dynamically requesting remote node data 添加动态请求远程节点数据后更新数据库的逻辑

---
+++ app/src/main/java/com/omni/wallet/data/NodeDao.java
@@ -0,0 +1,76 @@
+package com.omni.wallet.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.omni.wallet.common.NetworkType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NodeDao {
+    private static NodeDB mInstance;
+    public NodeDao(Context context){
+        mInstance = NodeDB.getInstance(context);
+    }
+
+    public void insertNode(String alias, String spayUrl, String nodeUrl, NetworkType networkType){
+        SQLiteDatabase db = mInstance.getWritableDatabase();
+        if (!db.isOpen()) {
+            return;
+        }
+        ContentValues values = new ContentValues();
+        values.put("alias", alias);
+        values.put("spayUrl", spayUrl);
+        values.put("nodeUrl", nodeUrl);
+        values.put("netType", networkType.toString());
+        db.insert("node_list", null, values);
+    }
+
+    public void clearTable(){
+        SQLiteDatabase db = mInstance.getWritableDatabase();
+        if (!db.isOpen()) {
+            return;
+        }
+        db.execSQL("delete from node_list");
+    }
+
+    public List<Node> getNodeListByNetType(NetworkType networkType){
+        List<Node> nodeList = new ArrayList<>();
+        String sql = "select * from node_list where netType = ?" ;
+        SQLiteDatabase db = mInstance.getReadableDatabase();
+        Cursor cursor = db.rawQuery(sql, new String[]{String.valueOf(networkType)});
+        while (cursor.moveToNext()){
+            String alias;
+            int aliasIndex = cursor.getColumnIndex("alias");
+            if (aliasIndex>=0){
+                alias = cursor.getString(aliasIndex);
+            }else{
+                alias = "";
+            }
+            String spayUrl;
+            int spayUrlIndex = cursor.getColumnIndex("spayUrl");
+            if (spayUrlIndex>=0){
+                spayUrl = cursor.getString(spayUrlIndex);
+            }else{
+                spayUrl = "";
+            }
+            String nodeUrl;
+            int nodeUrlIndex = cursor.getColumnIndex("nodeUrl");
+            if (nodeUrlIndex>=0){
+                nodeUrl = cursor.getString(nodeUrlIndex);
+            }else{
+                nodeUrl = "";
+            }
+            Node node = new Node(alias,spayUrl,nodeUrl,networkType);
+            nodeList.add(node);
+        }
+        cursor.close();
+//        db.close();
+        return nodeList;
+    }
+
+
+}
